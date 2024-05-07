2ce6d5fe7f25932edef3e1f2ae67169190b41559
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-05-07 16:42:15 +0530

    background svg add

--- ToDoPWA/src/App.jsx
+++ ToDoPWA/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import background from './assets/background.svg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
   <>
-
+    <div className='h-screen w-screen flex justify-center items-center'>
+      <div className='relative h-[80%]'>
+        <img src={background} alt="" className='h-full'/>
+      
+        <div className='h-[16.5%] w-full absolute top-0 flex justify-center items-center'>
+          <h1 className=' font-[Sinosans]'>ToDo List</h1>
+        </div>
+      </div>
+    </div>
   </>
   )
 }
