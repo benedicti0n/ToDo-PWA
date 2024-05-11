b2a56f212fb2efb668c32eb03d808c15246f1131
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-05-11 22:21:24 +0530

    AddTodo section

---
+++ ToDoPWA/src/components/AddTodo.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const AddTodo = () => {
+  return (
+    <div>
+        <div>
+            {/* Todo: input field */}
+        </div>
+
+        <div>
+            {/* ToDo: Add a dropdown menu for color */}
+        </div>
+
+        <div>
+            <button>Add</button>
+        </div>
+    </div>
+  )
+}
+
+export default AddTodo
