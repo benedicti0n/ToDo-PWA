8280cca87a728db47874d26bd9671482433a0316
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-05-11 22:04:52 +0530

    week data

---
+++ ToDoPWA/src/components/DayOfTheWeek.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DayOfTheWeek = () => {
+  return (
+    <div>DayOfTheWeek</div>
+  )
+}
---
+++ ToDoPWA/src/components/WeeklyData/week.js
@@ -0,0 +1,32 @@
+const dayOfWeek = [
+    {
+        id: 0,
+        nameOfDay: "Sun"
+    },
+    {
+        id: 1,
+        nameOfDay: "Mon"
+    },
+    {
+        id: 2,
+        nameOfDay: "Tue"
+    },
+    {
+        id: 3,
+        nameOfDay: "Wed"
+    },
+    {
+        id: 4,
+        nameOfDay: "Thur"
+    },
+    {
+        id: 5,
+        nameOfDay: "Fri"
+    },
+    {
+        id: 6,
+        nameOfDay: "Sat"
+    }
+];
+
+export default dayOfWeek;
