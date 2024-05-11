199c975a6d89ef1a349d1ee7d8042ea059e6620d
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-05-11 22:05:05 +0530

    week dot functionality

--- ToDoPWA/src/App.jsx
+++ ToDoPWA/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import background from './assets/background.svg'
+import dayOfWeek from './components/WeeklyData/week';
 
 function App() {
+  const date = new Date();
+  const currentDayIndex = date.getDay();
 
-  let currentDate = () => {
-    const currentDate = new Date();
+  console.log(currentDayIndex)
 
-
-    const year = currentDate.getFullYear(); // Get the current year (e.g., 2024)
-    const month = currentDate.getMonth() + 1; // Get the current month (0-indexed, so January is 0, February is 1, etc.)
-    const day = currentDate.getDate(); // Get the day of the month (1-31)
+  const currentDate = () => {
+    const year = date.getFullYear(); // Get the current year (e.g., 2024)
+    const month = date.getMonth() + 1; // Get the current month (0-indexed, so January is 0, February is 1, etc.)
+    const day = date.getDate(); // Get the day of the month (1-31)
 
     return `${day}/${month}/${year}`;
   }
@@ -24,10 +26,24 @@ function App() {
           <div className='top-0 h-[60%] w-full flex justify-center items-center'>
             <h1 className='font-[Italiana] text-[2rem]'>ToDo List</h1>
           </div>
-          <div className='h-[40%] w-full flex justify-between items-center'>
-            <h1 className='pl-8 font-[Italiana] text-[1rem]'>Date: {currentDate()}</h1>
-            <h1 className='pr-8 font-[Italiana] text-[1rem]'>ToDo List</h1>
+
+          <div className='h-[40%] w-[85%] flex justify-between items-center'>
+
+            <div className='h-[100%] w-[40%] flex items-center'>
+              <h1 className=' font-[Italiana] text-[1rem]'>Date: {currentDate()}</h1>
+            </div>
+
+            <div className='h-[100%] w-[60%] flex justify-around items-center'>
+              {dayOfWeek.map((day, index) => (
+                <div key={day.id} className='font-[Italiana] text-[1rem] relative'>
+                  <h1>{day.nameOfDay}</h1>
+                  {index === currentDayIndex && <div className=" w-[3px] h-[3px] bg-black rounded-full absolute right-1/2"></div>}
+                </div>
+              )) }
+              
+            </div>
           </div>
+
         </div>
       </div>
     </div>
